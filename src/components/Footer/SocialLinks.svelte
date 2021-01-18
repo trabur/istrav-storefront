f10e68d658d8c67b1e5974a51f354deeedb738f3
trabur <travis.burandt@gmal.com>
2021-01-18 14:36:43 -0600

    nav links

--- src/components/Footer/SocialLinks.svelte
+++ src/components/Footer/SocialLinks.svelte
@@ -1,4 +1,10 @@
-<ul class="right" style="margin-top: 0.3em;">
+<ul class="right hide-on-med-and-down" style="margin-top: 0.3em;">
+  <li><a href="#" data-target="slide-out"><i class="material-icons">camera_alt</i></a></li>
+  <li><a href="#" data-target="slide-out"><i class="material-icons">facebook</i></a></li>
+  <li><a href="#" data-target="slide-out"><i class="material-icons">videocam</i></a></li>
+  <li><a href="#" data-target="slide-out"><i class="material-icons">forum</i></a></li>
+</ul>
+<ul class="right hide-on-large-only" style="">
   <li><a href="#" data-target="slide-out"><i class="material-icons">camera_alt</i></a></li>
   <li><a href="#" data-target="slide-out"><i class="material-icons">facebook</i></a></li>
   <li><a href="#" data-target="slide-out"><i class="material-icons">videocam</i></a></li>

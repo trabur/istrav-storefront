fdf0ad5cd7d1af90b80b9dbacecf1fa48438a95e
trabur <travis.burandt@gmal.com>
2021-01-17 20:36:19 -0600

    featured

---
+++ src/components/Banner/Featured.svelte
@@ -0,0 +1,50 @@
+<script>
+  import { onMount } from 'svelte';
+
+  onMount(() => {
+		new Splide( '#secondary-slider', {
+      fixedWidth: 400,
+		  height: 300,
+      rewind: true,
+      gap: 10,
+		  cover: true,
+      pagination: true,
+    }).mount();
+  })
+</script>
+<br />
+<br />
+<div class="row">
+  <div class="col s0 m1"></div>
+  <div class="col s12 m10">
+    <h3 class="title">FEATURED</h3>
+    <div id="secondary-slider" class="splide">
+      <div class="splide__track">
+        <ul class="splide__list">
+          <li class="splide__slide">
+            <img src="coffee-cup.jpg">
+          </li>
+          <li class="splide__slide">
+            <img src="coffee-cup.jpg">
+          </li>
+          <li class="splide__slide">
+            <img src="coffee-cup.jpg">
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<style>
+.title {
+  font-size: 2rem;
+  font-weight: 900;
+}
+
+.splide__slide img {
+	width: 100%;
+	height: auto;
+}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,8 @@
 <script>
-	import Counter from '$components/Counter.svelte'
 	import Navigation from '$components/MainMenu/Navigation.svelte'
 	import FrontPageBanner from '$components/Banner/FrontPage.svelte'
   import Footer from '$components/Footer/Main.svelte'
+  import Featured from '$components/Banner/Featured.svelte'
 	
 	let name = import.meta.env.SNOWPACK_PUBLIC_NAME
 	let apiUri = import.meta.env.SNOWPACK_PUBLIC_API_URI
@@ -14,13 +14,7 @@
 
 <Navigation />
 <FrontPageBanner />
-<main>
-	<h1>Hello world!</h1>
-
-	<Counter/>
-	<p>Visit the <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte apps.</p>
-</main>
-
+<Featured />
 <Footer>
 	<a href="/" class="breadcrumb">Home</a>
 	<a href="/" class="breadcrumb">Welcome</a>
@@ -29,37 +23,5 @@
 <style>
 	:root {
 		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-	}
-
-	main {
-		text-align: center;
-		padding: 1em;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4rem;
-		font-weight: 100;
-		line-height: 1.1;
-		margin: 4rem auto;
-		max-width: 14rem;
-	}
-
-	p {
-		max-width: 14rem;
-		margin: 2rem auto;
-		line-height: 1.35;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			max-width: none;
-		}
-
-		p {
-			max-width: none;
-		}
 	}
 </style>

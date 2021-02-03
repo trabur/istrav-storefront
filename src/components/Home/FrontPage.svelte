92a8f45386704dc4d6386509040e10e08e31d63e
trabur <travis.burandt@gmal.com>
2021-02-02 18:26:31 -0600

    banner center middle

--- src/components/Home/FrontPage.svelte
+++ src/components/Home/FrontPage.svelte
@@ -34,40 +34,46 @@
 <div class="parallax-container">
   <div class="parallax">
     {#if image && media}
-      <div class="image" style={`background-image: url(./media/${media}/${image});`}></div>
+      <div class="image" style={`background-image: url(./media/${media}/${image});`}>
+        <div class="announce">
+          <div class="hide-on-med-and-down">
+            <span class="first"><h1 class="text">{announce.line1}</h1></span><br />
+            <span class="second"><h1 class="text">{announce.line2}</h1></span>
+          </div>
+          <div class="hide-on-large-only">
+            <span class="first"><h1 class="text small">{announce.line1}</h1></span><br />
+            <span class="second"><h1 class="text small">{announce.line2}</h1></span>
+          </div>
+          <a href={`${announce.buttonUrl}`} class="waves-effect waves-light btn-large" style="margin-top: 0.5em;">{announce.buttonName}</a>
+        </div>
+      </div>
     {/if}
   </div>
 </div>
 
-<div class="announce">
-  <span class="first hide-on-med-and-down"><h1 class="text">{announce.line1}</h1></span><br />
-  <span class="second hide-on-med-and-down"><h1 class="text">{announce.line2}</h1></span><br />
-  <span class="first hide-on-large-only"><h1 class="text small">{announce.line1}</h1></span><br />
-  <span class="second hide-on-large-only"><h1 class="text small">{announce.line2}</h1></span><br />
-  <a href={`${announce.buttonUrl}`} class="waves-effect waves-light btn-large" style="margin-top: 0.5em;">{announce.buttonName}</a>
-</div>
 
 
 <style>
 
 .image {
   width: 100%;
-  height: 700px;
+  height: 100vh;
   background-position: center center;
   background-repeat: no-repeat;
   background-size: cover;
 }
 
 .parallax-container {
-  height: 700px;
+  height: 100vh;
 }
 
 .announce {
-  overflow: hidden;
-  margin-top: -475px;
-  height: 300px;
   text-align: center;
-  margin-bottom: 175px;
+  width: 100%;
+  position: absolute;
+  top: 50%;
+  -ms-transform: translateY(-50%);
+  transform: translateY(-50%);
 }
 
 .first,

515e7678c4225c648d4fbfdc2be2b4f3cf7dd0bb
trabur <travis.burandt@gmal.com>
2021-02-17 14:13:43 -0600

    category

--- src/components/Category/Banner.svelte
+++ src/components/Category/Banner.svelte
@@ -4,52 +4,75 @@
 
   // objects
   export let categoryId
-  let products = []
-  let media
+  let uploads
+  let domain
+  let state
+  let domainId
   let image
 
-	onMount(() => {
-    media = window.location.host
+	onMount(async () => {
+    // fetch
+    let appId
+    domain = window.location.host
+    state = 'production'
 
-    // products
-    fetch(`./media/${media}/data.json`, {
-      method: 'GET',
-      headers: {
-        'Cache-Control': 'no-cache',
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
+    // pick an app to show for local development
+    if (domain.includes('localhost:3000')) {
+      domain = 'istrav.dimension.click'
+    }
+    // set appId from domain 
+    if (domain.includes('dimension.click')) {
+      // for subdomains such as http://istrav.dimension.click
+      let endpoint = domain.split('.')[0]
+      let esEndpoint = await scripts.tenant.apps.getEndpoint(endpoint)
+      if (esEndpoint.payload.success === true) {
+        appId = esEndpoint.payload.data.id
+        domainId = esEndpoint.payload.data.domain
+        uploads = esEndpoint.payload.data.uploads
+      } else {
+        alert(esEndpoint.payload.reason)
       }
-    })
-      .then(r => r.json())
-      .then(value => {
-        let category = value.shopByCategory.filter((value) => {
-          return value.slug === categoryId
-        })[0]
-        console.log('category:', category)
-        image = category.image
-      })
+    } else {
+      // for custom domains such as https://istrav.com
+      let esOne = await scripts.tenant.apps.getOne(domain, state)
+      if (esOne.payload.success === true) {
+        appId = esOne.payload.data.id
+        domainId = esOne.payload.data.domain
+        uploads = esOne.payload.data.uploads
+      } else {
+        alert(esOne.payload.reason)
+      }
+    }
+    // get the products
+    let esProducts = await scripts.store.categories.getOne(appId, categoryId)
+    if (esProducts.payload.success === true) {
+      image = esProducts.payload.data.image
+    } else {
+      alert(esProducts.payload.reason)
+    }
+    // console.log('category products', items)
   })
 </script>
 
 <div class="banner">
-  {#if image && media}
-    <div class="image" style={`background-image: url(./media/${media}/${image});`}></div>
+  {#if image}
+    <div class="image" style={`background-image: url(https://rawcdn.githack.com/${uploads}/${domainId}/${state}/categories/${categoryId}/${image});`}></div>
   {/if}
 </div>
 
 
 <style>
-.image {
-  width: 100%;
-  height: 350px;
-  background-position: center center;
-  background-repeat: no-repeat;
-  background-size: cover;
-  box-shadow: inset 0px 10px 10px -10px #000;
-}
+  .image {
+    width: 100%;
+    height: 350px;
+    background-position: center center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    box-shadow: inset 0px 10px 10px -10px #000;
+  }
 
-.banner {
-  height: 350px;
-  position: relative;
-}
+  .banner {
+    height: 350px;
+    position: relative;
+  }
 </style>
--- src/components/Category/ListProducts.svelte
+++ src/components/Category/ListProducts.svelte
@@ -4,26 +4,53 @@
 
   // objects
   export let categoryId
-  let products = []
-  let media
+  let items = []
+  let uploads
+  let domain
+  let state
+  let domainId
 
-	onMount(() => {
-    media = window.location.host
+	onMount(async () => {
+    // fetch
+    let appId
+    domain = window.location.host
+    state = 'production'
 
-    // products
-    fetch(`./media/${media}/categories/${categoryId}.json`, {
-      method: 'GET',
-      headers: {
-        'Cache-Control': 'no-cache',
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
+    // pick an app to show for local development
+    if (domain.includes('localhost:3000')) {
+      domain = 'istrav.dimension.click'
+    }
+    // set appId from domain 
+    if (domain.includes('dimension.click')) {
+      // for subdomains such as http://istrav.dimension.click
+      let endpoint = domain.split('.')[0]
+      let esEndpoint = await scripts.tenant.apps.getEndpoint(endpoint)
+      if (esEndpoint.payload.success === true) {
+        appId = esEndpoint.payload.data.id
+        domainId = esEndpoint.payload.data.domain
+        uploads = esEndpoint.payload.data.uploads
+      } else {
+        alert(esEndpoint.payload.reason)
       }
-    })
-      .then(r => r.json())
-      .then(value => {
-        console.log('products:', value)
-        products = value
-      })
+    } else {
+      // for custom domains such as https://istrav.com
+      let esOne = await scripts.tenant.apps.getOne(domain, state)
+      if (esOne.payload.success === true) {
+        appId = esOne.payload.data.id
+        domainId = esOne.payload.data.domain
+        uploads = esOne.payload.data.uploads
+      } else {
+        alert(esOne.payload.reason)
+      }
+    }
+    // get the products
+    let esProducts = await scripts.store.categories.getProducts(appId, categoryId)
+    if (esProducts.payload.success === true) {
+      items = esProducts.payload.data
+    } else {
+      alert(esProducts.payload.reason)
+    }
+    // console.log('category products', items)
   })
 </script>
 
@@ -32,17 +59,17 @@
   <div class="col s12 m10">
     <h3 class="title">{categoryId}</h3>
     <div class="masonry">
-      {#each products as item (item.slug)}
+      {#each items as item (item.slug)}
         <div class="item">
           <a href={`/products/${item.slug}`}>
-            <div class="image" style={`background-image: url(/media/${media}/products/${item.slug}/${item.image});`}>
+            <div class="image" style={`background-image: url(https://rawcdn.githack.com/${uploads}/${domainId}/${state}/products/${item.slug}/${item.image});`}>
             </div>
             <h5 style="color: #111; margin: 0.5em 0 0 0;">{item.name}</h5>
           </a>
         </div>
       {/each}
     </div>
-    {#if products.length === 0}
+    {#if items.length === 0}
       <p>no products...</p>
     {/if}
   </div>
@@ -50,43 +77,43 @@
 <!-- hello list products {JSON.stringify(products, null, 2)} -->
 
 <style>
-.title {
-  font-size: 2rem;
-  font-weight: 900;
-  text-transform: uppercase;
-}
-
-.image {
-  width: 100%;
-  height: 200px;
-  background-position: center center;
-  background-repeat: no-repeat;
-  background-size: cover;
-}
-
-.masonry {
-  width: 100%;
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr;
-  grid-gap: 2em;
-}
-
-.item {
-  padding: 0;
-  margin: 0;
-}
-
-/* Masonry on medium-sized screens */
-@media only screen and (max-width: 1023px) and (min-width: 768px) {
-  .masonry {
-    grid-template-columns: 1fr 1fr 1fr;
+  .title {
+    font-size: 2rem;
+    font-weight: 900;
+    text-transform: uppercase;
   }
-}
 
-/* Masonry on small screens */
-@media only screen and (max-width: 767px) and (min-width: 0px) {
-  .masonry {
-    grid-template-columns: 1fr 1fr;
+  .image {
+    width: 100%;
+    height: 200px;
+    background-position: center center;
+    background-repeat: no-repeat;
+    background-size: cover;
   }
-}
+
+  .masonry {
+    width: 100%;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    grid-gap: 2em;
+  }
+
+  .item {
+    padding: 0;
+    margin: 0;
+  }
+
+  /* Masonry on medium-sized screens */
+  @media only screen and (max-width: 1023px) and (min-width: 768px) {
+    .masonry {
+      grid-template-columns: 1fr 1fr 1fr;
+    }
+  }
+
+  /* Masonry on small screens */
+  @media only screen and (max-width: 767px) and (min-width: 0px) {
+    .masonry {
+      grid-template-columns: 1fr 1fr;
+    }
+  }
 </style>

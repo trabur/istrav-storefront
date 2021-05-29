756d20b450a20e1d2c40087e8c64335e0185c3dc
istrav <travis.burandt@gmail.com>
2021-05-28 19:58:06 -0500

    dockerfile

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -70,8 +70,23 @@
     console.log(domain, backend, headless, io)
     
     // configure backend
-    istrav.tenant.apps.init({ host: backend })
     istrav.account.carts.init({ host: backend })
+    istrav.account.orders.init({ host: backend })
+    istrav.account.users.init({ host: backend })
+    istrav.app.blocks.init({ host: backend })
+    istrav.app.faq.init({ host: backend })
+    istrav.app.menus.init({ host: backend })
+    istrav.app.pages.init({ host: backend })
+    istrav.channel.guides.init({ host: backend })
+    istrav.channel.playlists.init({ host: backend })
+    istrav.channel.videos.init({ host: backend })
+    istrav.store.categories.init({ host: backend })
+    istrav.store.collections.init({ host: backend })
+    istrav.store.products.init({ host: backend })
+    istrav.subscription.licenses.init({ host: backend })
+    istrav.subscription.plans.init({ host: backend })
+    istrav.tenant.apps.init({ host: backend })
+    istrav.tenant.members.init({ host: backend })
 
     // pick an app to show for local development
     if (domain.includes('localhost:7000')) {
